72e4cb488d01ae6ce7bbd17073322c115a4f3d76
Leonid Tikhonov <tikleonid2006@yandex.ru>
2026-01-25 19:52:46 +0400

    done but some problems with checkout session

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
+import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from 'eslint-config-prettier/flat'
 
 export default defineConfig([
@@ -22,6 +23,11 @@ export default defineConfig([
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
 
   skipFormatting,
 ])
